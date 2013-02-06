251fd42ee2da94db9e0ce77be9ea51169a0af07e
mj15 <>
2013-02-06 14:23:37 -0700

    Stuff

--- Programming3W/src/mj/helloWorld/helloWorld.java
+++ Programming3W/src/mj/helloWorld/helloWorld.java
@@ -4,12 +4,15 @@
  * @author mjohansen15
  */
 
+//Version 1.00
+
 package mj.helloWorld; 
 
 public class helloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("lololololololol!");
     }
 
 }
